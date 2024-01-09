1697ae128a5f50619aea87dc6c541b89f3f19132
Nicolas Santos <nicollas.ricardo@hotmail.com>
2024-01-09 07:19:08 -0300

    fim da aula

--- ModificadoresDeAcesso/src/pacote1/Classe1.java
+++ ModificadoresDeAcesso/src/pacote1/Classe1.java
@@ -1,4 +1,25 @@
 package pacote1;
 
 public class Classe1 {
+    public String getPropriedadePrivada() {
+        return propriedadePrivada;
+    }
+
+    private String propriedadePrivada;
+
+    public String propriedadePublica;
+
+    String propriedadeDefault;
+
+    protected String propriedadeProtected;
+
+    public void testeNicolas(){
+
+    }
+    public void setPropriedadePrivada(String propriedadePrivada) {
+        this.propriedadePrivada = propriedadePrivada;
+    }
+
+
+
 }
--- ModificadoresDeAcesso/src/pacote2/Classe3.java
+++ ModificadoresDeAcesso/src/pacote2/Classe3.java
@@ -1,4 +1,20 @@
 package pacote2;
 
-public class Classe3 {
+import pacote1.Classe1;
+import pacote1.Classe2;
+
+public class Classe3 extends Classe2 {
+
+    private String propriedadePrivada;
+
+    public String propriedadePublica;
+
+    String propriedadeDefault;
+
+    protected String propriedadeProtected;
+    public static void main(String [] args) {
+        Classe1 classe1 = new Classe1();
+        classe1.testeNicolas();
+
+    }
 }
